b847b445e632fdfc3f9766097edef2d89d7974f5
soheilqk <soheil.q.k@gmail.com>
2024-04-11 14:54:35 +0330

    Initial Commit

---
+++ AzerbaijaniTurkishWithArabicAlphabetKeyboard/AzerbaijaniTurkishWithArabicAlphabetKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzerbaijaniTurkishWithArabicAlphabetKeyboardApp.swift
+//  AzerbaijaniTurkishWithArabicAlphabetKeyboard
+//
+//  Created by Soheil on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AzerbaijaniTurkishWithArabicAlphabetKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
